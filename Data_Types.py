a3515aa95c8ea8c64992a9dfba35faba5a5ffa9d
Andres Salazar G <jandysg3101@hotmail.com>
2021-10-07 19:27:50 -0500

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+value1 = 12
+value2 = 4.0
+value3 = "is the best place to learn and practice coding!"
+
+# Read and save an integer, double, and String to your variables.
+input1 = int(input())
+input2 = float(input())
+input3 = str(input())
+
+# Print the sum of both integer variables on a new line.
+print (i + input1)
+print (d + input2)
+print (s + input3)
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.

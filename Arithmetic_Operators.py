780a30a94ec87d1a55c92934a72b3973c22072ad
Andres Salazar G <jandysg3101@hotmail.com>
2021-10-12 19:03:08 -0500

    Add files via upload

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    summ = a + b
+    difference = a - b
+    product = a * b
+
+    print(summ)
+    print(difference)
+    print(product)
